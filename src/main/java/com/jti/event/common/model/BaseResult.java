0b55a30f1185981d11257d9d53ca9cbf857da5fc
juryanghan <jr.han@tckdigital.com>
2022-05-26 10:56:37 +0900

    2022.05.26 스프링부트 멀티 모듈

--- src/main/java/com/jti/event/common/model/BaseResult.java
+++
@@ -1,9 +0,0 @@
-package com.jti.event.common.model;
-import lombok.Data;
-
-@Data
-public class BaseResult {
-	private String ResultCode = "0000";
-	private String ResultMsg = "정상";
-	private Object ResultData;
-}
--- src/main/resources/static/front/js/menu.js
+++
@@ -1,9 +0,0 @@
-if(location.pathname.indexOf("product") != -1 || location.pathname.indexOf("order") != -1){
-	$(".navbar-nav").find("#product").addClass("active");
-}
-else if(location.pathname.indexOf("mypage") != -1){
-	$(".navbar-nav").find("#mypage").addClass("active");
-}
-else if(location.pathname.indexOf("faq") != -1){
-	$(".navbar-nav").find("#faq").addClass("active");
-}
