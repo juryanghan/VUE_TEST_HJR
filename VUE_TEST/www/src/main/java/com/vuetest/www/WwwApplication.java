15d1939bc745944118e7a70ff3d05e875d6439d4
juryanghan <jr.han@tckdigital.com>
2022-05-26 17:03:58 +0900

    2022.05.26 스프링부트 멀티 모듈

---
+++ VUE_TEST/www/src/main/java/com/vuetest/www/WwwApplication.java
@@ -0,0 +1,11 @@
+package com.vuetest.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WwwApplication.class, args);
+    }
+}
